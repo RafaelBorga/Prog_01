7f7127bf617d4a1670e9ec55a88af51b53236581
RafaelBorga <rafasimonborga@hotmail.com>
2024-04-06 20:27:00 -0300

    Atividade 3 - Operadores

---
+++ 240406_Atividade_3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+int x = 1;
+int y = 1;
+char op = ' ';
+float res = 0;
+
+Console.WriteLine("Insira seu operador matemático (+|-|*|/):");
+op = char.Parse(Console.ReadLine());
+
+if (op == '+')
+{
+    for (x = 1;x <= 9; x++)
+    {
+        for (y = 1;y <= 9; y++)
+            {
+            res = x+y;
+            Console.WriteLine($"{x} + {y} = {res}");
+            }
+        Console.WriteLine("-------------");
+    }
+}
+
+if (op == '-')
+{
+    for (x = 1;x <= 9; x++)
+    {
+        for (y = 1;y <= 9; y++)
+            {
+            res = x-y;
+            Console.WriteLine($"{x} - {y} = {res}");
+            }
+        Console.WriteLine("-------------");
+    }
+}
+
+if (op == '*')
+{
+    for (x = 1;x <= 9; x++)
+    {
+        for (y = 1;y <= 9; y++)
+            {
+            res = x*y;
+            Console.WriteLine($"{x} * {y} = {res}");
+            }
+        Console.WriteLine("-------------");
+    }
+}
+
+if (op == '/')
+{
+    for (x = 1;x <= 9; x++)
+    {
+        for (y = 1;y <= 9; y++)
+            {
+            res = x/y;
+            Console.WriteLine($"{x} / {y} = {res}");
+            }
+        Console.WriteLine("-------------");
+    }
+}
